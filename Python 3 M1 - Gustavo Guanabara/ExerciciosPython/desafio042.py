43eb8cb9d4ca50baa7e30194eeba52fc674c575b
Perciliano <luizperciliano@gmail.com>
2022-07-07 13:49:17 -0300

    Update desafio042.py

--- Python 3 M1 - Gustavo Guanabara/ExerciciosPython/desafio042.py
+++ Python 3 M1 - Gustavo Guanabara/ExerciciosPython/desafio042.py
@@ -1,8 +1,8 @@
 # Complementar do desafio 035
 
-segmento1 = int(input("Digite o segmento de reta 1: "))
-segmento2 = int(input("Digite o segmento de reta 2: "))
-segmento3 = int(input("Digite o segmento de reta 3: "))
+segmento1 = float(input("Digite o segmento de reta 1: "))
+segmento2 = float(input("Digite o segmento de reta 2: "))
+segmento3 = float(input("Digite o segmento de reta 3: "))
 
 if segmento1 == segmento2 and segmento1 == segmento3:
     print("Equilátero")
@@ -10,8 +10,6 @@ elif segmento1 == segmento2 or segmento2 == segmento3:
     print("Isóceles")
 elif segmento1 != segmento2 or segmento2 != segmento3:
     print("Escaleno")
-
-
 
 print(f"Segemento 1 -  {segmento1}\nSegmento 2 - {segmento2}\nSegmento 3 - {segmento3}")
 
